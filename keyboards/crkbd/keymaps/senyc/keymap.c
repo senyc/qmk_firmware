858b77b7ed9b346755c0d852688f66ac097140ce
kyler <95313103+senyc@users.noreply.github.com>
2024-08-03 09:35:24 -0400

    feat: update keymaps again

--- keyboards/crkbd/keymaps/senyc/keymap.c
+++ keyboards/crkbd/keymaps/senyc/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
       KC_TRNS,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                   KC_6,      KC_7,     KC_8,     KC_9,      KC_0,     KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
-      KC_TRNS,  KC_PGUP,  KC_PGDN,  KC_TRNS,  KC_TILDE,  KC_PIPE,               KC_BSLS,  KC_GRAVE,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+      KC_TRNS,  KC_BSLS,  KC_PIPE,  KC_PLUS,  KC_EQL,  KC_MPLY,                KC_UNDS,  KC_MINUS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
                                       //`--------------------------'  `--------------------------'
@@ -44,11 +44,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Top layer is identical as layer 2 (trns only work for active layers e.g. the default layer)
     [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------+--------.
-      KC_TRNS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,                KC_CIRC,   KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_TRNS,
+      KC_TRNS,  KC_EXLM,  KC_AT,    KC_HASH,   KC_DLR,   KC_PERC,               KC_CIRC,  KC_AMPR,   KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
-      KC_TRNS,  KC_TRNS,  KC_PLUS,  KC_LBRC,  KC_RBRC,  KC_EQL,                 KC_MINUS,  KC_LCBR,  KC_RCBR,  KC_UNDS,  KC_TRNS,  KC_TRNS,
+      KC_TRNS,  KC_TILDE,  KC_GRAVE,  KC_LBRC,   KC_RBRC,  KC_5,                KC_6,  KC_LCBR,   KC_RCBR,  KC_9,  KC_TRNS,  KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
-      KC_TRNS,  KC_PGUP,  KC_PGDN,  KC_TRNS,  KC_TILDE,  KC_PIPE,               KC_BSLS,  KC_GRAVE,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+      KC_TRNS,  KC_BSLS,  KC_PIPE,  KC_PLUS,  KC_EQL,   KC_MPLY,                KC_UNDS,  KC_MINUS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
                                       //`--------------------------'  `--------------------------'
@@ -56,11 +56,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // page up page down home end
     [3] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_TRNS,  KC_F1,   KC_F2,    KC_F3,    KC_F4,    KC_F5,                   KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,   KC_TRNS,
+      KC_PWR,  KC_F1,   KC_F2,    KC_F3,    KC_F4,      KC_F5,                  KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,   KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS,  KC_PWR,  KC_MUTE,  KC_MPRV,  KC_MPLY,  KC_MNXT,                 KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  KC_PSCR,  KC_TRNS,
+      KC_TRNS,  KC_VOLD,  KC_VOLU,  KC_MPRV,  KC_MNXT,  KC_MUTE,                KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  KC_PSCR,  KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS,  KC_F11,  KC_F12,   KC_HOME,  KC_END,   KC_TRNS,                 KC_TRNS,  KC_VOLD,  KC_VOLU,  KC_TRNS,   KC_TRNS,  KC_TRNS,
+      KC_TRNS,  KC_F11,  KC_F12,  KC_HOME,  KC_END,    KC_TRNS,                 KC_TRNS,  KC_PGDN,  KC_PGUP,  KC_TRNS,   KC_TRNS,  KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
                                       //`--------------------------'  `--------------------------'
