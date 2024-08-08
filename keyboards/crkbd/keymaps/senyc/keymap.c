14d33cd0bdb0f9d6a52375c7a20cc8e2d79fb2b8
kyler <95313103+senyc@users.noreply.github.com>
2024-08-08 19:48:07 -0400

    feat: stop changing the key order

--- keyboards/crkbd/keymaps/senyc/keymap.c
+++ keyboards/crkbd/keymaps/senyc/keymap.c
@@ -32,36 +32,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
     [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------+--------.
-      KC_TRNS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,                KC_CIRC,  KC_AMPR,   KC_ASTR,  KC_LPRN,  KC_RPRN,   KC_TRNS,
+      KC_TRNS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,                KC_CIRC,  KC_AMPR,   KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
-      KC_TRNS,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                   KC_6,     KC_7,      KC_8,     KC_9,      KC_0,     KC_TRNS,
+      KC_TRNS,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                   KC_6,     KC_7,      KC_8,     KC_9,     KC_0,     KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
-      KC_TRNS,  KC_BSLS,  KC_PIPE,  KC_PLUS,  KC_EQL,   KC_MPLY,                KC_UNDS,  KC_MINUS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+      KC_TRNS,  KC_BSLS,  KC_PIPE,  KC_PLUS,  KC_EQL,   KC_MPLY,                KC_UNDS,  KC_MINUS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
                                           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
                                       //`--------------------------'  `--------------------------'
   ),
     // Top layer is identical as layer 2 (trns only work for active layers e.g. the default layer)
     [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------+--------.
-      KC_TRNS,  KC_EXLM,   KC_AT,     KC_HASH,  KC_DLR,   KC_PERC,              KC_CIRC,  KC_AMPR,   KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_TRNS,
+      KC_TRNS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,                KC_CIRC,  KC_AMPR,   KC_ASTR,  KC_LPRN,   KC_RPRN,  KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
-      KC_TRNS,  KC_TILDE,  KC_GRAVE,  KC_LBRC,  KC_RBRC,  KC_5,                 KC_6,     KC_LCBR,   KC_RCBR,  KC_9,     KC_TRNS,  KC_TRNS,
+      KC_TRNS,  KC_LBRC,  KC_RCBR,  KC_LCBR,  KC_RCBR,  KC_GRAVE,               KC_LEFT,  KC_DOWN,   KC_UP,    KC_RIGHT,  KC_TRNS,  KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
-      KC_TRNS,  KC_BSLS,   KC_PIPE,   KC_PLUS,  KC_EQL,   KC_MPLY,              KC_UNDS,  KC_MINUS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+      KC_TRNS,  KC_BSLS,  KC_PIPE,  KC_PLUS,  KC_EQL,   KC_TILDE,               KC_UNDS,  KC_MINUS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
                                           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
                                       //`--------------------------'  `--------------------------'
   ),
     // page up page down home end
     [3] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_PWR,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                  KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,   KC_TRNS,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS,  KC_VOLD,  KC_VOLU,  KC_MPRV,  KC_MNXT,  KC_MUTE,                KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  KC_PSCR,  KC_TRNS,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS,  KC_F11,   KC_F12,   KC_HOME,  KC_END,   KC_TRNS,                KC_TRNS,  KC_PGDN,  KC_PGUP,  KC_TRNS,   KC_TRNS,  KC_TRNS,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------+--------.
+      KC_PWR,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
+      KC_TRNS,  KC_MSTP,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_BRID,                KC_BRIU,  KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_PSCR,  KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
+      KC_TRNS,  KC_F11,   KC_F12,   KC_HOME,  KC_END,   KC_TRNS,                KC_PGDN,  KC_PGUP,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
                                           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
                                       //`--------------------------'  `--------------------------'
   ),
