7aa023e982f2391c5640898876f8caa703c31825
kyler <95313103+senyc@users.noreply.github.com>
2024-08-08 20:01:16 -0400

    fix: correct some keybind bugs

--- keyboards/crkbd/keymaps/senyc/keymap.c
+++ keyboards/crkbd/keymaps/senyc/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                               KC_H,  KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                               KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  RALT_T,
+      KC_LCTL,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                               KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_RALT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,  MO(1),  KC_SPC,     KC_TAB,  MO(2),  KC_BSPC
                                       //`--------------------------'  `--------------------------'
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
       KC_TRNS,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                   KC_6,     KC_7,      KC_8,     KC_9,     KC_0,     KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
-      KC_TRNS,  KC_BSLS,  KC_PIPE,  KC_PLUS,  KC_EQL,   KC_MPLY,                KC_UNDS,  KC_MINUS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+      KC_TRNS,  KC_BSLS,  KC_PIPE,  KC_PLUS,  KC_EQL,   KC_TILDE,               KC_UNDS,  KC_MINUS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
                                           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
                                       //`--------------------------'  `--------------------------'
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------+--------.
       KC_TRNS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,                KC_CIRC,  KC_AMPR,   KC_ASTR,  KC_LPRN,   KC_RPRN,  KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
-      KC_TRNS,  KC_LBRC,  KC_RCBR,  KC_LCBR,  KC_RCBR,  KC_GRAVE,               KC_LEFT,  KC_DOWN,   KC_UP,    KC_RIGHT,  KC_TRNS,  KC_TRNS,
+      KC_TRNS,  KC_LBRC,  KC_RBRC,  KC_LCBR,  KC_RCBR,  KC_GRAVE,               KC_LEFT,  KC_DOWN,   KC_UP,    KC_RIGHT,  KC_TRNS,  KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
       KC_TRNS,  KC_BSLS,  KC_PIPE,  KC_PLUS,  KC_EQL,   KC_TILDE,               KC_UNDS,  KC_MINUS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
